d1c7f2a904dae1880e21772cf0002fb50a1facf4
iosub <1330995+iosub@users.noreply.github.com>
2024-07-23 04:07:06 +0200

    ggg

--- examples/ex_data_extractor.py
+++ examples/ex_data_extractor.py
@@ -7,9 +7,11 @@ llm=OllamaChat(model="llama3:8b-instruct-q8_0")
 info = read_pdf("C:/Ia/docs/0/biele05.pdf")
 
 # It will reply in proper json since we set reply_as_json to True
-de = DataExtractor(llm, reply_as_json=True)
+de = DataExtractor(llm, reply_as_json=True,temperature=0)
 
-resp = de.ask(info, ["name", "email", "phone", "address","items"])
+#resp = de.ask(info, ["name", "email", "phone", "address","items"])
+resp = de.ask(info, ["name", "email", "phone", "address","items","all data"])
+
 print(resp)
 
 # We can then convert to a proper python object if we wish
